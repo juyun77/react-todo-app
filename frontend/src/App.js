5865473dec9fca11c8ad471b7d90da2e2b2b54b0
juyun <juyun571@naver.com>
2025-01-26 01:39:49 +0900

    backend server랑 연결

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./style/Sass.scss";
+import ListContainer from "./components/ListContainer";
+
+function App() {
+  console.log(process.env.REACT_APP_API_SERVER);
+  return (
+    <>
+      <h1>Todo app 만들기</h1>
+      <ListContainer />
+    </>
+  );
+}
+
+export default App;
