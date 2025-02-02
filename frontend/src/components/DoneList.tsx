f0481f7a6a6ef6a90d71bd8306cd1ff38675cade
juyun <juyun571@naver.com>
2025-02-02 17:07:24 +0900

    typesctipt migration

--- frontend/src/components/DoneList.tsx
+++ frontend/src/components/DoneList.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
+import { ReduxState } from "../types/types";
 
 export default function DoneList() {
   // 완료 목록 불러오고
-  const doneList = useSelector((state) => state.todo.list).filter(
+  const doneList = useSelector((state: ReduxState) => state.todo.list).filter(
     (el) => el.done === true
   );
 
-  console.log(doneList);
+  // console.log(doneList);
   return (
     <section>
       <h3>완료 목록</h3>
